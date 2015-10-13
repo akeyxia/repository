d32e202c1fb770cc8f42dc9fee56e57ceabb72fa
akeyxia <akeyxia@foxmail.com>
2015-10-13 23:02:35 +0800

    utils: tstamp: support to config serial port from input parameters

--- utils/tstamp/logging.sh
+++ utils/tstamp/logging.sh
@@ -1,5 +1,10 @@
 #! /bin/bash
 
 __CUR_DIR=`dirname $0`
+__PORT=0
 
-sudo $__CUR_DIR/tstamp < /dev/ttyUSB0
+if [ $# -eq 1 ]; then
+	__PORT=$1
+fi
+
+sudo $__CUR_DIR/tstamp < /dev/ttyUSB$__PORT
--- utils/tstamp/tty_setting.sh
+++ utils/tstamp/tty_setting.sh
@@ -1,3 +1,9 @@
 #! /bin/bash
 
-stty -F /dev/ttyUSB0 speed 115200 -parenb -cstopb cread clocal -crtscts
+__PORT=0
+
+if [ $# -eq 1 ]; then
+	__PORT=$1
+fi
+
+stty -F /dev/ttyUSB$__PORT speed 115200 -parenb -cstopb cread clocal -crtscts
